df60ea844e6aa395eb6548e8a9e95901e6957831
isaiahchol33 <isaiahchol33@gmail.com>
2026-04-12 08:44:42 +0200

    Delete app directory

--- app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .wallet import Wallet
-from .transaction import Transaction
-from .currency import Currency
-from .wallet_history import WalletHistory
-
-__all__ = ['User', 'Wallet', 'Transaction', 'Currency', 'WalletHistory']
--- app/models/currency.py
+++
@@ -1,6 +0,0 @@
-#app/models/currency.py 
-from app import db
-class Currency(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    symbol = db.Column(db.String(5), nullable=False)
