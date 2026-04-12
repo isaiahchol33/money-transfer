c0621e9f76f623ebe9db21f5f7bb2ec9188ccbff
isaiahchol33 <isaiahchol33@gmail.com>
2026-04-12 12:09:15 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -107,8 +107,7 @@ def create_app():
     @app.errorhandler(500)
     def server_error(error):
         return render_template('500.html'), 500
-        
-        with app.app_context():
+    with app.app_context():
         if not hasattr(app, "db_initialized"):
             db.create_all()
             app.db_initialized = True
