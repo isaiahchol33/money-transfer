ce79e483d98927c7141d47b3e9bb4ff3355251fa
isaiahchol33 <isaiahchol33@gmail.com>
2026-04-17 03:08:58 +0200

    final upate

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
+import eventlet
+eventlet.monkey_patch()
+
 import os
 from app import create_app, socketio
 
 app = create_app()
 
-# 👇 IMPORTANT: expose Flask app for gunicorn
-application = app
-
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     socketio.run(
         app,
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
-        debug=True,
-        use_reloader=False
+        port=port,
+        debug=False
     )
