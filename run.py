63ee82d3ec2425fb8976e9e66b9087feab0f1c47
isaiahchol33 <isaiahchol33@gmail.com>
2026-04-17 02:34:50 +0200

    remove socketio from run.py

--- run.py
+++ run.py
@@ -1,18 +1,10 @@
-import os
-from app import create_app, socketio
+from app import create_app
 
-# Create Flask app
 app = create_app()
 
-# =========================
-# 🔥 DEVELOPMENT ONLY
-# =========================
 if __name__ == "__main__":
-    socketio.run(
-        app,
+    app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
-        debug=True,
-        use_reloader=False,
-        log_output=True
+        port=5000,
+        debug=True
     )
