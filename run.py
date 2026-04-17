adec86164b89a6938ede864f6f91498b23ba938d
isaiahchol33 <isaiahchol33@gmail.com>
2026-04-17 02:18:22 +0200

    remove eventlet completely

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-import eventlet
-eventlet.monkey_patch()
-
+import os
 from app import create_app, socketio
 
 app = create_app()
@@ -9,8 +7,8 @@ if __name__ == "__main__":
     socketio.run(
         app,
         host="0.0.0.0",
-        port=5000,
+        port=int(os.environ.get("PORT", 5000)),
         debug=True,
-        use_reloader=False,   # 🔥 prevents duplicate socket connections in dev
+        use_reloader=False,
         log_output=True
     )
